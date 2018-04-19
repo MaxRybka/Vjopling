5c5e504cc700d95b75eaf8b9b4f9eb03f7ff5c55
Max <maximrmk@gmail.com>
2018-04-19 10:12:39 +0300

    Client/Server changes

--- src/Client.java
+++ src/Client.java
@@ -5,29 +5,100 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.ImageIcon;
 
 public class Client {
 	static int port = 8080;
 	static Socket socket;
+	HashMap<String,Goods> goods;
 
 	public static void main(String args[]) throws Exception {
-		createStuff("second", "Description", 1, "Category", new File("Image\\background.jpg"));
-//		getStuff("Name");
+//		createStuff("second", "Description", 1, "Category", new File("Image\\background.jpg"));
+		goodsMassive();
+
 	}
 
 	public static void createStuff(String name, String description, int quantity, String category, File image) {
 		CreateStuffThread cs = new CreateStuffThread(name, description, quantity, category, image);
 		Thread thread = new Thread(cs);
 		thread.start();
+		
+	}
+
+	public static HashMap<String,Goods> goodsMassive() {
+		System.out.println("Goods1");
+		ShowAll sa = new ShowAll();
+		Thread thread = new Thread(sa);
+		thread.start();
+		System.out.println("Goods2");
+		return sa.getGoods();
+
+	}
+
+}
+
+class ShowAll implements Runnable {
+	HashMap<String, Goods> stuff = new HashMap<>();
+	String name,description,category;
+	Socket socket;
+	
+	public HashMap<String, Goods> getGoods() {
+		return stuff;
+	}
+
+	@Override
+	public void run() {
+		try {
+			socket = new Socket("192.168.31.21", 8080);
+			PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			pw.println("All");	
+			readGoods();
+			pw.close();
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	
+	private void readGoods() {
+		try {
+			DataInputStream dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+			DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+			while(dis.readBoolean()) {
+				String name = dis.readUTF();
+				String description = dis.readUTF();
+				int quantity = dis.readInt();
+				String category = dis.readUTF();
+				int len = dis.readInt();
+				System.out.println(len);
+				byte[] imageB = new byte[len];
+				if(len>0) {
+				    
+				    dis.readFully(imageB, 0, imageB.length); // read the message
+				}
+				ImageIcon image = new ImageIcon(imageB);
+				//Goods stuff = new Goods();
+			}
+			
+			dis.close();
+			dos.close();
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
 
 class CreateStuffThread implements Runnable {
@@ -48,10 +119,9 @@ class CreateStuffThread implements Runnable {
 		try {
 			Socket socket = new Socket("192.168.31.21", 8080);
 			PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			// BufferedReader br = new BufferedReader(new
+			// InputStreamReader(socket.getInputStream()));
 			pw.println("Create");
-
-			
 
 			try {
 				DataInputStream dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
@@ -65,20 +135,19 @@ class CreateStuffThread implements Runnable {
 				dos.flush();
 				dos.writeUTF(category);
 				dos.flush();
-				
-				
+
 				dos.writeUTF(image.getName());
 				dos.flush();
 
 				int n = 0;
 				dos.writeInt((int) image.length());
-				 System.out.println(""+image.length());
+				System.out.println("" + image.length());
 				byte[] buf = new byte[(int) image.length()];
 
 				System.out.println(image.getName());
 
 				FileInputStream fis = new FileInputStream(image);
-				
+
 				while ((n = fis.read(buf)) != -1) {
 					dos.write(buf, 0, n);
 					dos.flush();
@@ -88,7 +157,7 @@ class CreateStuffThread implements Runnable {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
